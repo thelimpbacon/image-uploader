52ca6e89b5a61bf0213f9e1788482d7c946b74c6
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-04-26 12:47:34 +0200

    add image preview

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,38 @@
+import { useRef, useState } from "react";
 import ProgressBar from "../ProgressBar";
 import s from "./Home.module.css";
 
 const Home = () => {
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleFileChange = (e) => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    reader.onload = (e) => setPreview(e.target.result);
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
     <div className={s.root}>
+      <input
+        ref={imageInputRef}
+        type="file"
+        onChange={handleFileChange}
+        hidden
+      />
+
+      <button onClick={() => imageInputRef.current.click()}>
+        Upload image
+      </button>
+
       <div className={s.imageContainer}>
-        <ProgressBar progress={69} />
+        {preview && <img className={s.imagePreview} src={preview} />}
+        <div className={s.progressBarContainer}>
+          <ProgressBar progress={69} />
+        </div>
       </div>
     </div>
   );
